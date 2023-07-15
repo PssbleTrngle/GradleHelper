8123721994e7d53e9d5a2edb48807ad8c0e5a44a
Niklas Widmann <nik@somethingcatchy.net>
2023-07-15 20:48:22 +0200

    Create create-fabric.build.kts

---
+++ repositories/create-fabric.build.kts
@@ -0,0 +1,43 @@
+repositories {
+    maven {
+        url = uri("https://mvn.devos.one/snapshots/")
+        content {
+            includeGroup("com.simibubi.create")
+            includeGroup("io.github.fabricators_of_create.Porting-Lib")
+            includeGroup("io.github.tropheusj")
+            includeGroup("com.tterrag.registrate_fabric")
+        }
+    }
+    maven {
+        url = uri("https://maven.jamieswhiteshirt.com/libs-release")
+        content {
+            includeGroup("com.jamieswhiteshirt")
+        }
+    }
+    maven {
+        url = uri("https://jitpack.io")
+        content {
+            includeGroup("com.github.LlamaLad7")
+            includeGroup("com.github.Chocohead")
+            includeGroup("com.github.llamalad7.mixinextras")
+        }
+    }
+    maven {
+        url = uri("https://maven.blamejared.com")
+        content {
+            includeGroup("com.faux.ingredientextension")
+        }
+    }
+    maven {
+        url = uri("https://raw.githubusercontent.com/Fuzss/modresources/main/maven/")
+        content {
+            includeGroup("net.minecraftforge")
+        }
+    }
+    maven {
+        url = uri("https://maven.terraformersmc.com/")
+        content {
+            includeGroup("dev.emi")
+        }
+    }
+}
