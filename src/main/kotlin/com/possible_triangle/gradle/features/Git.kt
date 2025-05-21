a4936873eebfa7ccbc158d40ae260d555160e311
Niklas Widmann <nik@somethingcatchy.net>
2025-05-21 15:25:49 +0200

    skip hook setup if .git folder is missing

--- src/main/kotlin/com/possible_triangle/gradle/features/Git.kt
+++ src/main/kotlin/com/possible_triangle/gradle/features/Git.kt
@@ -4,13 +4,11 @@ import com.possible_triangle.gradle.GradleHelperPlugin
 import org.gradle.api.Project
 import java.io.File
 
-fun copyPreCommitHook() {
+fun Project.setupGitExtension() {
     val resource = GradleHelperPlugin::class.java.classLoader.getResource("hooks/pre-commit.sh")
         ?: throw NullPointerException("Unable to find pre commit hook")
     val text = resource.readText()
-    File(".git/hooks/pre-commit").writeText(text)
-}
-
-fun Project.setupGitExtension() {
-    copyPreCommitHook()
+    val to = File(".git/hooks/pre-commit")
+    if (to.parentFile.exists()) to.writeText(text)
+    else logger.warn("could not locate git directory at ${to.parentFile.parentFile.absoluteFile}")
 }
