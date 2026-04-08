f204efec70d46ccccb659566dccab8efdf107c0e
Niklas Widmann <nik@somethingcatchy.net>
2026-04-08 11:54:49 +0200

    add mod description property & API to allow additional properties to be added

--- plugins/core/src/main/kotlin/com/possible_triangle/gradle/ModExtension.kt
+++ plugins/core/src/main/kotlin/com/possible_triangle/gradle/ModExtension.kt
@@ -5,6 +5,7 @@ import com.possible_triangle.gradle.features.loaders.IncludedImpl
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.hasPlugin
 import org.gradle.kotlin.dsl.property
@@ -20,6 +21,7 @@ interface ModExtension {
     val name: Property<String>
     val version: Property<String>
     val author: Property<String>
+    val description: Property<String>
     val minecraftVersion: Property<String>
     val releaseType: Property<String>
     val repository: Property<String>
@@ -27,6 +29,13 @@ interface ModExtension {
 
     val libraries: Included
     val mods: Included
+
+    val additional: AdditionalProperties
+}
+
+interface AdditionalProperties {
+    fun add(key: String, value: String)
+    fun add(key: String, value: Provider<String>)
 }
 
 internal open class ModExtensionImpl(project: Project) : ModExtension {
@@ -34,10 +43,44 @@ internal open class ModExtensionImpl(project: Project) : ModExtension {
     override val name: Property<String> = project.objects.property()
     override val version: Property<String> = project.objects.property()
     override val author: Property<String> = project.objects.property()
+    override val description: Property<String> = project.objects.property()
     override val minecraftVersion: Property<String> = project.objects.property()
     override val releaseType: Property<String> = project.objects.property()
     override val repository: Property<String> = project.objects.property()
     override val mavenGroup: Property<String> = project.objects.property()
     override val libraries: Included = IncludedImpl(project, project.parent?.mod?.libraries)
     override val mods: Included = IncludedImpl(project, project.parent?.mod?.mods)
+    override val additional: AdditionalPropertiesImpl = AdditionalPropertiesImpl(project)
+}
+
+internal class AdditionalPropertiesImpl(private val project: Project) : AdditionalProperties {
+    internal val values = mutableMapOf<String, Provider<String>>()
+
+    override fun add(key: String, value: String) = add(key, project.provider { value })
+
+    override fun add(key: String, value: Provider<String>) {
+        values[key] = value
+    }
+}
+
+internal fun ModExtensionImpl.resolveProperties(): Map<String, String> {
+    val mcVersionRange = minecraftVersion.map { "[$it,)" }
+
+    val providers = mapOf(
+        "version" to version,
+        "mod_version" to version,
+        "mod_name" to name,
+        "mod_id" to id,
+        "mod_author" to author,
+        "mod_description" to description,
+        "repository" to repository,
+        "minecraft_version" to minecraftVersion,
+        "mc_version" to minecraftVersion,
+        "minecraft_version_range" to mcVersionRange,
+        "mc_version_range" to mcVersionRange,
+    ) + additional.values
+
+    return providers
+        .filterValues { it.isPresent }
+        .mapValues { it.value.get() }
 }
