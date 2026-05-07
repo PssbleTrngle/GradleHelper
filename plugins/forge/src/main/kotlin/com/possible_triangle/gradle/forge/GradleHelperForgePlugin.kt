b8f62c094678e29a58363dd1f34f032bd99a6e70
Niklas Widmann <nik@somethingcatchy.net>
2026-05-07 22:09:45 +0200

    modify publication after evaluation

--- plugins/forge/src/main/kotlin/com/possible_triangle/gradle/forge/GradleHelperForgePlugin.kt
+++ plugins/forge/src/main/kotlin/com/possible_triangle/gradle/forge/GradleHelperForgePlugin.kt
@@ -51,6 +51,10 @@ class GradleHelperForgePlugin : LoaderPlugin(ForgeLoaderSpecifics) {
                 }
             }
         }
+
+        modifyPublication {
+            removeDependencies(this)
+        }
     }
 
     private fun Project.configureModSourceSets(config: ForgeExtensionImpl) {
@@ -171,10 +175,6 @@ class GradleHelperForgePlugin : LoaderPlugin(ForgeLoaderSpecifics) {
                     add("thedarkcolour:kotlinforforge:$it")
                 }
             }
-        }
-
-        modifyPublication {
-            removeDependencies(this)
         }
     }
 }
