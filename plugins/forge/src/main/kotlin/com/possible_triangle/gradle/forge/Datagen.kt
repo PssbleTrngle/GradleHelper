4a5dbd6655e903c5bc1d747f6b6b1212c4ed5414
Niklas Widmann <nik@somethingcatchy.net>
2026-07-24 22:34:09 +0200

    execute forge data gen task in run/data

--- plugins/forge/src/main/kotlin/com/possible_triangle/gradle/forge/Datagen.kt
+++ plugins/forge/src/main/kotlin/com/possible_triangle/gradle/forge/Datagen.kt
@@ -3,6 +3,7 @@ package com.possible_triangle.gradle.forge
 import com.possible_triangle.gradle.mod
 import net.neoforged.moddevgradle.legacyforge.dsl.LegacyForgeExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.the
 
@@ -12,6 +13,8 @@ internal fun Project.configureDatagenRun() {
     configure<LegacyForgeExtension> {
         if (config.enabledDataGen) {
             runs.named("data") {
+                gameDirectory = project.file("run/data")
+
                 val existingResources = listOf("--existing", config.existingResources.path)
                 val existingMods = config.existingMods.flatMap { listOf("--existing-mod", it) }
                 val dataGenArgs =
